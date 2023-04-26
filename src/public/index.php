591a660e150921a24a37f68d9da28dc17d81e252
RobertLuann <robluann@gmail.com>
2023-04-26 17:42:37 -0300

    Mudou coisa pra caralho

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="pt-BR">
     <head>
         <title>Aprendendo PHP</title>
     </head>
@@ -68,6 +68,84 @@
                 echo 'Pagina não encontrada!';
                 break;  
         };
+
+        echo '<br>';
+
+        $tanque = 1;
+
+        echo $tanque <= 1 ? 'Abastecer agora <br>' : 'Combustivel OK <br>';
+
+        $statustanque = $tanque <= 1 ? false : true;
+
+        if($statustanque) echo 'Combustivel OK';
+        else echo 'Procurar um posto para abastecer.';
+
+        echo '<br>';
+
+        // $vegetais = array('jerimum','batata','tomate');
+
+        // echo 'Hoje vou cozinhar ' . $vegetais[0] . '<br>';
+        // echo 'Hoje vou cozinhar ' . $vegetais[1] . '<br>';
+        // echo 'Hoje vou cozinhar ' . $vegetais[2] . '<br>';
+
+        // echo 'Quantidade de vegetais: ' . count($vegetais);
+
+        // $vegetais[10] = 'repolho';
+        // echo "<br>" . $vegetais[10] . '<br>';
+        // print_r($vegetais);
+
+        $vegetais = array(
+            'jerimum' => '',
+            'batata' => '',
+            'tomate' => '');
+        
+            foreach($vegetais as $chave=>$valor){
+                echo 'Hoje vou cozinhar ' . $chave . "<img src='$valor' > <br>";
+            };
+
+        $estoqueCarros = array(
+            array('Palio', 10, 125),
+            array('Uno', 15, 200),
+            array('Gol', 4, 38)
+        );
+
+        echo 'Temos: ' . $estoqueCarros[0][1] . ' ' . $estoqueCarros[0][0] . '<br>Vendidos: ' . $estoqueCarros[0][2];
+
+        echo '<br>';
+
+        print_r($estoqueCarros);
+
+        echo '<br>';
+
     ?>
+    <br>
+    <form method="post" action="index.php">
+        Nome: <input type="text" name="nome">
+        <input type="submit">
+    </form> 
+    <?php
+    if(empty($_POST['nome'])){
+        echo 'Nome é um campo obrigatório.';
+    } else {
+        echo $_POST['nome'] . ', obrigado por se inscrever!';
+    };
+
+    ?>
+
+
+    <?php
+        $data = date('d/m/Y');
+        $hora = date('H');
+        if($hora < 12){
+            $mensagem = "Bom dia! <img src='https://i.pinimg.com/736x/48/81/de/4881de7d28d76349af48a21da9776035--pasta.jpg' >";
+        } elseif(date('H') > 11 && date('H') < 18) {
+            $mensagem = "Boa tarde! <img src='https://frasesparapostar.com.br/wp-content/uploads/2022/08/frases-de-boa-tarde.jpg'>";
+            $mensagem .= 'Aceita chá?';
+        } elseif(date('H') > 17) {
+            $mensagem = "Boa noite! <img src='https://mensagensfeliz.com.br/wp-content/uploads/2021/04/familia-mensagens-de-boa-noite-para-facebook-e-whatsapp-1.jpg'>";
+        };
+    ?>
+    <br>
+    Olá! <?php echo $mensagem; ?>
     </body>
 </html>
