1988382a715100f40f3029d94c8acb92553f5298
RobertLuann <robluann@gmail.com>
2023-05-24 17:46:46 -0300

    MT COISA

---
+++ src/public/exemplo/caixa_de_entrada.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php 
+        $usuarios = array();
+        $usuarios['Maria']['nome'] = 'Maria da Silva';
+        $usuarios['Maria']['id'] = 5974;
+        $usuarios['Joao']['nome'] = 'Joao Alfredo';
+        $usuarios['Joao']['id'] = 3618;
+        $userLogin = $_POST['login'];
+        $userId = $usuarios[$userLogin]['id'];
+    ?>
+    <h1>Olá! <?php echo $usuarios[$userLogin]['nome']; ?></h1>
+    <form action="apagar.php" method="POST">
+        <ul>
+            <li>Email 1</li>
+            <li>Email 2</li>
+            <input type="hidden" name="id" value="<?php echo $userId; ?>">
+        </ul>
+    </form>
+    <a href="https://www.google.com/search?q=php">Google</a>
+</body>
+</html>
--- src/public/exemplo/index.php
+++ src/public/exemplo/index.php
@@ -1,11 +1,18 @@
-<?php 
-    require('live.php');
-
-    include('header.html');
-
-    echo '<h1> Olá </h1>';
-
-    echo '<h2>' . multiplica(2,4) . "</h2>";
-
-    include('footer.html');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="caixa_de_entrada.php" method="POST">
+        Login: <input type="text" name="login">
+        Senha: <input type="password" name="senha">
+        <br>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
